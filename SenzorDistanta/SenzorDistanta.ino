d100026416fdaf551e2490ad76e1ecf990b0f46a
soniadeca28 <sonia.deca@student.upt.ro>
2022-05-13 22:21:27 +0300

    Updated communication between nodemcu and arduino

---
+++ SenzorDistanta/SenzorDistanta.ino
@@ -0,0 +1,28 @@
+#define echopin 8
+#define trigpin 9
+float distance;
+void setup() {
+  pinMode(echopin,INPUT);
+  pinMode(trigpin,OUTPUT);
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,LOW);
+  distance=pulseIn(echopin,HIGH);
+  distance=distance/58;
+  Serial.begin(9600);
+}
+
+void loop() {
+   
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,LOW);
+  distance=pulseIn(echopin,HIGH);
+  distance=distance/58; //because the pulse travels at 58 microseconds/cm, and distance is measured in microseconds
+  Serial.println(distance);
+  
+}
